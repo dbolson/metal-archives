a6b8657daec3c02f8a67c4515dbe3c802db8be50
Danny <dbolson@gmail.com>
2011-01-29 16:31:58 -0800

    Can search album links in a paginated results page.

--- lib/metal-archives.rb
+++ lib/metal-archives.rb
@@ -7,6 +7,12 @@ module MetalArchives
       @agent = Mechanize.new
     end
 
+    def search_by_year(year=Time.now.year)
+      @agent.get("http://metal-archives.com/advanced.php?release_year=#{year}")
+      #search_results_html = File.open(File.dirname(__FILE__) + '/../spec/html/search_results.html')
+      #Nokogiri::HTML(search_results_html)
+    end
+
     def paginated_result_links(year=nil)
       results = @agent.search_by_year(year)
       results.search('body table:nth-child(2n) tr:first-child a').collect do |link|
@@ -14,12 +20,11 @@ module MetalArchives
       end
     end
 
-    private
-
-    def search_by_year(year=Time.now.year)
-      @agent.get("http://metal-archives.com/advanced.php?release_year=#{year}")
-      #search_results_html = File.open(File.dirname(__FILE__) + '/../spec/html/search_results.html')
-      #Nokogiri::HTML(search_results_html)
+    def album_links_from_url(url)
+      page = @agent.get(url)
+      results = page.search('body table:nth-child(2n) tr td:nth-child(3n) a').collect do |link|
+        link['href']
+      end
     end
   end
 end
--- spec/metal-archives_spec.rb
+++ spec/metal-archives_spec.rb
@@ -13,8 +13,7 @@ describe "MetalArchives" do
       @mechanize.stub!(:get).and_return(@search_results)
       agent = MetalArchives::Agent.new
 
-      #agent.search_by_year.should == search_results
-      agent.send(:search_by_year).should == @search_results
+      agent.search_by_year.should == @search_results
     end
 
     context "with a results page" do
@@ -27,6 +26,27 @@ describe "MetalArchives" do
           links << "/advanced.php?band_name=&band_status=&genre=&themes=&origin=0&location=&bandLabel=&release_name=&release_type=&label=&release_year=2011&p=#{i}"
         end
         agent.paginated_result_links.should == links
+      end
+
+      it "should find the album links" do
+        @mechanize.stub!(:search_by_year).and_return(@search_results)
+        @mechanize.stub!(:get).and_return(@search_results)
+        agent = MetalArchives::Agent.new
+
+        links = [
+          "release.php?id=296061", "release.php?id=295756", "release.php?id=294429", "release.php?id=295451", "release.php?id=295197",
+          "release.php?id=289824", "release.php?id=295519", "release.php?id=295457", "release.php?id=288298", "release.php?id=296048",
+          "release.php?id=290116", "release.php?id=295059", "release.php?id=291931", "release.php?id=296081", "release.php?id=295301",
+          "release.php?id=294242", "release.php?id=294032", "release.php?id=295436", "release.php?id=295797", "release.php?id=294481",
+          "release.php?id=290911", "release.php?id=295988", "release.php?id=295717", "release.php?id=293534", "release.php?id=295111",
+          "release.php?id=290063", "release.php?id=294761", "release.php?id=293839", "release.php?id=295202", "release.php?id=294083",
+          "release.php?id=294702", "release.php?id=265276", "release.php?id=293421", "release.php?id=295295", "release.php?id=293910",
+          "release.php?id=295567", "release.php?id=296037", "release.php?id=296064", "release.php?id=288749", "release.php?id=290749",
+          "release.php?id=295806", "release.php?id=294017", "release.php?id=296098", "release.php?id=294092", "release.php?id=295551",
+          "release.php?id=290740", "release.php?id=295410", "release.php?id=293189", "release.php?id=296140", "release.php?id=291295"
+        ]
+        agent.album_links_from_url(agent.paginated_result_links.first).should == links
+
       end
     end
   end
