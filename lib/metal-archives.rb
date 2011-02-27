81b01750a26b0d0664ed5bfbb0878ea69fad5558
Danny <dbolson@gmail.com>
2011-02-26 20:17:44 -0800

    Return nil if unable to access site when finding album information from a url.

--- lib/metal-archives.rb
+++ lib/metal-archives.rb
@@ -63,18 +63,19 @@ module MetalArchives
     # album's release date
     # album's release type (full-length, demo, split, DVD, etc.)
     def album_from_url(url)
-      page = @agent.get(SITE_URL + '/' + url)
-      page.encoding = 'iso-8859-1' if !page.nil? && page.encoding != 'iso-8859-1' # needed for foreign characters
-      band_and_album = page.search('body table tr:first-child .tt').text
-
-      # these fields can be in one of the following forms, so we need to find the specific fields appropriately:
-      # "\n\t\t", "Demo", ", NazgÃ»l Distro & Prod.", "", "2011", "\t\t\t"
-      # "\n\t\t", "Demo", ", Deific Mourning", "", "\n\n\t\tJanuary ", "2011", "\t\t\t"
-      # "Full-length", ", ARX Productions", "", "\n\n\t\tFebruary 25th, ", "2011", "\t\t\t"
       begin
+        page = @agent.get(SITE_URL + '/' + url)
+        page.encoding = 'iso-8859-1' if !page.nil? && page.encoding != 'iso-8859-1' # needed for foreign characters
+        band_and_album = page.search('body table tr:first-child .tt').text
+
+        # these fields can be in one of the following forms, so we need to find the specific fields appropriately:
+        # "\n\t\t", "Demo", ", NazgÃ»l Distro & Prod.", "", "2011", "\t\t\t"
+        # "\n\t\t", "Demo", ", Deific Mourning", "", "\n\n\t\tJanuary ", "2011", "\t\t\t"
+        # "Full-length", ", ARX Productions", "", "\n\n\t\tFebruary 25th, ", "2011", "\t\t\t"
         album_fields = page.search('body table:nth-child(2n) tr:first-child > td:first-child').first.children
       rescue Exception => e
         puts "\nError accessing metal-archives.com's album information: #{e}"
+        return nil
       end
 
       {
